097aa17f350432ab1a65379bedf2b23018e1785e
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-05-12 17:07:36 -0400

    BMI calulator codewars - first try

---
+++ BMIcalculator.js
@@ -0,0 +1,17 @@
+// write a function that calculates BMI (weight/height^2) also return values in reference to the given bmi
+
+function bmi(weight, height) {
+    let bmi = weight / Math.pow(height, 2)
+    if (bmi <= 18.5){
+      return "Underweight";
+    }
+    else if (bmi <= 25.0){
+      return "Normal";
+    }
+    else if (bmi <=30.0){
+      return "Overweight";
+    }
+    else if (bmi > 30.0){
+      return "Obese";
+    }
+  }
